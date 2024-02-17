677d1f6bd74b793688cf17ba94b1acdb95bc7e1d
Cezary-Ambroziak <157055208+Cezary-Ambroziak@users.noreply.github.com>
2024-02-17 20:18:34 +0100

    feat: add architecture (#4)
    * initial setup
    
    * Initial architecture

---
+++ src/MoviePlatform.Runner/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviePlatform.Runner;
+
+public class Movie
+{
+    public string Id { get; set; }
+    public string Type { get; set; }
+    public string Title { get; set; }
+    public string Director { get; set; }
+    public string Cast { get; set; }
+    public string Country { get; set; }
+    public string DateAdded { get; set; }
+    public string ReleaseYear { get; set; }
+    public string Rating { get; set; }
+    public string Duration { get; set; }
+    public string ListedIn { get; set; }
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return
+        $"{Id,-2} | " +
+        $"{Type,-5} | " +
+        $"{(Title.Length > 20 ? Title.Substring(0, 20) + "..." : Title),-23} | " +
+        $"{Director,-20} | " +
+        $"{(Cast.Length > 20 ? Cast.Substring(0, 20) + "..." : Cast),-15} | " +
+        $"{Country,-5} | " +
+        $"{DateAdded,-4} | " +
+        $"{ReleaseYear,-7} | " +
+        $"{Rating,-10} | " +
+        $"{Duration,-10} | " +
+        $"{ListedIn,-10} | " +
+        $"{Description,-10} | ";
+    }
+}
+
---
+++ src/MoviePlatform.Runner/Program.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+
+namespace MoviePlatform.Runner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string csvPath = @"C:/Users/Czarek/Desktop/MoviePlatform/netflix_titles.csv";
+
+        //string csvPath2 = @"C: \Users\Czarek\Desktop\FullStack - Zasoby\googleplaystore1.csv";
+
+        var moviesNetflix = LoadGoogleAps(csvPath);
+
+        //var moviesnetflix = loadgoogleaps(csvpath);
+
+        Display(moviesNetflix);
+    }
+
+    static void Display(IEnumerable<Movie> moviesNetflix)
+    {
+        foreach (var movie in moviesNetflix)
+        {
+            Console.WriteLine(movie);
+        }
+    }
+
+    public static List<Movie> LoadGoogleAps(string csvPath)
+    {
+        using var reader = new StreamReader(csvPath);
+        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ",",
+            NewLine = ";;",
+            BadDataFound = null
+
+        };
+        using var csv = new CsvReader(reader, configuration);
+        csv.Context.RegisterClassMap<MoviesMap>();
+
+        var records = csv.GetRecords<Movie>().Take(50).ToList();
+        return records;
+    }
+}
